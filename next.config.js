d9fcc3fc337ad99628b286b608e9e086f250ad21
iRexus <101460160+dikacakep@users.noreply.github.com>
2025-12-30 08:57:29 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,12 @@
-// next.config.js
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   async rewrites() {
     return [
-      {
-        source: '/ios',
-        destination: '/public/ios.txt',
-      },
-      {
-        source: '/android',
-        destination: '/public/android.txt',
-      },
-      {
-        source: '/windows',
-        destination: '/public/windows.txt',
-      },
-    ]
+      { source: '/ios', destination: '/api/file/ios' },
+      { source: '/android', destination: '/api/file/android' },
+      { source: '/windows', destination: '/api/file/windows' },
+    ];
   },
-}
+};
+
+module.exports = nextConfig;
