4844aa9e5984b0b5544779863e85df632effa4c2
iRexus <101460160+dikacakep@users.noreply.github.com>
2025-12-30 14:38:18 +0700

    Update GarudaPS.js

--- api/GarudaPS.js
+++ api/GarudaPS.js
@@ -6,6 +6,6 @@ export default function handler(req, res) {
   const content = fs.readFileSync(filePath, 'utf8');
   
   res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-  res.setHeader('Content-Disposition', 'attachment; filename="vhost.txt"');
+  res.setHeader('Content-Disposition', 'attachment; filename="garudaps.txt"');
   res.status(200).send(content);
 }
